ddba11b1cd47da56277376bbe30b1e23b1979e78
addierabbitson <39174595+addierabbitson@users.noreply.github.com>
2019-05-02 12:20:26 +1000

    Added some AI enemies and working UI detection.

---
+++ SqueakySpy/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform firstNode;
+    private Transform firstPos;
+    private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+        navAgent = GetComponent<NavMeshAgent>();
+        firstPos = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, firstNode.position) < 2) {
+            navAgent.destination = firstPos.position;
+        }
+        if (Vector3.Distance(transform.position, firstPos.position) < 2)
+        {
+            navAgent.destination = firstNode.position;
+        }
+    }
+}
---
+++ SqueakySpy/Assets/Scripts/UIDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDetection : MonoBehaviour {
+
+    public GameObject progressionBar;
+    public float health;
+
+
+    private void Update() {
+        progressionBar.GetComponent<Image>().fillAmount = health * 0.01f;
+    }
+}
